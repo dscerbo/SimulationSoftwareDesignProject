6ba3f1d21294e692584460668a1f52a5b55bbc67
leedumaliang <lduma002@odu.edu>
2016-04-14 15:45:39 -0400

    Started adjacency matrix implementation

---
+++ SoftwareGroupProject_MSIM331/SoftwareGroupProject_MSIM331/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+	int numVertices;
+	std::cout << "# of vertices";
+	std::cin >> numVertices; 
+	
+	//2D array (its an array to pointers to arrays)
+	int **adjMatrix = new int*[numVertices];
+	for (int i = 0; i < numVertices; ++i)
+		adjMatrix[i] = new int[numVertices]; 
+
+}
